40774e45020ddca88d96d25c1155ce31aef3b291
Victor Chen <chenvcvictor@gmail.com>
2024-07-19 14:24:44 -0700

    Revert "added project card--personal website"
    This reverts commit 5458a716925bbcb3eebb98fb7c03e8e8ef1ddd1f.

--- src/components/About.js
+++ src/components/About.js
@@ -8,40 +8,45 @@ Layout Idea:
     Contact Info
 */
 
-import * as React from "react";
-import { Container, Typography, Grid, Box, CardMedia } from "@mui/material";
-import { aboutText } from "/src/textContent";
 
-// probably 2 columns
+import * as React from 'react';
+import { Container, Typography, Grid, Toolbar, Box, CardMedia } from '@mui/material';
+import { aboutText } from '/src/textContent'
 
-/*
-To Do:
-- Update about text
+import profilePicUrl from '/src/images/Photo_KokoCraterTrail_VictorChen.jpeg'
 
-*/
+// probably 2 columns 
 
 function About() {
-  const profilePicUrl = "/src/images/Photo_KokoCraterTrail_VictorChen.jpeg";
 
-  return (
-    <>
-      <Box id="about" />
-      <Container maxWidth="lg" sx={{}}>
-        <Box sx={{ height: "50px" }} />
-        <Typography variant="h3" gutterBottom>
-          About
-        </Typography>
-        <Grid container spacing={12}>
-          <Grid item xs={12} md={5}>
-            <CardMedia component="img" src={profilePicUrl} />
-          </Grid>
-          <Grid item xs={12} md={7}>
-            <Typography>{aboutText}</Typography>
-          </Grid>
-        </Grid>
-      </Container>
-    </>
-  );
+    return (
+        <>
+        <Box id="about" />
+        <Container maxWidth="lg" sx={{}}>
+            <Box sx={{ height: '50px' }} /> 
+            <Typography variant = 'h3' gutterBottom>
+                About
+            </Typography>
+            <Grid container spacing = {12}>
+                <Grid item xs = {12} md = {5}>
+                    <CardMedia 
+                        component="img"
+                        src = {profilePicUrl}
+                    />
+                        
+                </Grid>
+                <Grid item xs = {12} md = {7}>
+                    <Typography> 
+                    {aboutText}
+                    </Typography>
+                </Grid>
+            </Grid>
+        </Container>
+        
+        </>
+    )
 }
 
+
 export default About;
+
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,152 +1,149 @@
-import * as React from "react";
-import MenuIcon from "@mui/icons-material/Menu";
-import {
-  AppBar,
-  Box,
-  Toolbar,
-  Container,
-  Typography,
-  IconButton,
-  Menu,
-  MenuItem,
-  Button,
-} from "@mui/material";
-import "./styles.css";
+import * as React from 'react';
+import MenuIcon from '@mui/icons-material/Menu';
+import { AppBar, Box, Toolbar, Container, Typography, IconButton, Menu, MenuItem, Button } from '@mui/material'
 
-// { name: 'About', href: '#about' },
-// { name: 'Projects', href: '#projects' },
-// { name: 'Experience', href: '#experience' },
-// { name: 'Resume', href: '#resume' }
-const navItems = ["Home", "About", "Projects", "Experience", "Resume"];
+    // { name: 'About', href: '#about' },
+    // { name: 'Projects', href: '#projects' },
+    // { name: 'Experience', href: '#experience' },
+    // { name: 'Resume', href: '#resume' }
+const navItems = ['Home', 'About', 'Projects', 'Experience', 'Resume'];
 
 function NavBar() {
-  const [elementNav, setElementNav] = React.useState(null);
+    const [anchorElNav, setAnchorElNav] = React.useState(null);
 
-  const handleOpenNavMenu = (event) => {
-    setElementNav(event.currentTarget);
-  };
+    const handleOpenNavMenu = (event) => {
+        setAnchorElNav(event.currentTarget);
+    };
 
-  const handleCloseNavMenu = () => {
-    setElementNav(null);
-  };
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
 
-  const handleScrollTo = (section) => {
-    const sectionElement = document.getElementById(section);
-    if (sectionElement) {
-      const offset = 50;
-      const elementPosition = sectionElement.getBoundingClientRect().top;
-      const offsetPosition = elementPosition + window.scrollY - offset;
+    const handleScrollTo = (section) => {
+        const sectionElement = document.getElementById(section);
+        if (sectionElement) {
+          const offset = 50;
+          const elementPosition = sectionElement.getBoundingClientRect().top;
+          const offsetPosition = elementPosition + window.scrollY - offset;
+      
+          window.scrollTo({
+            top: offsetPosition,
+            behavior: 'smooth'
+          });
+        }
+      };
 
-      window.scrollTo({
-        top: offsetPosition,
-        behavior: "smooth",
-      });
-    }
-  };
+    return (
+        <>
+            <AppBar position="fixed">
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
 
-  return (
-    <>
-      <AppBar position="fixed">
-        <Container maxWidth="xl">
-          <Toolbar disableGutters>
-            {/* Full screen for Header */}
-            <Typography
-              variant="h6"
-              noWrap
-              component="a"
-              href="/"
-              sx={{
-                mr: 2,
-                display: { xs: "none", md: "flex" },
-                fontFamily: "sans-serif",
-                fontWeight: 700,
-                letterSpacing: ".3rem",
-                color: "inherit",
-                textDecoration: "none",
-              }}
-            >
-              Victor Chen
-            </Typography>
-
-            {/* Full screen menu */}
-            <Box sx={{ flexGrow: 10, display: { xs: "none", md: "flex" } }} />
-            <Box sx={{ display: { xs: "none", md: "flex" } }}>
-              {navItems.map((item) => (
-                <Button
-                  key={item}
-                  onClick={() => {
-                    handleCloseNavMenu();
-                    handleScrollTo(item.toLowerCase());
-                  }}
-                  sx={{ ml: 1, mr: 1, my: 2, color: "white", display: "block" }}
+                {/* Full screen for Header */}
+                <Typography
+                    variant="h6"
+                    noWrap
+                    component="a"
+                    href="/"
+                    sx={{
+                    mr: 2,
+                    display: { xs: 'none', md: 'flex' },
+                    fontFamily: 'sans-serif',
+                    fontWeight: 700,
+                    letterSpacing: '.3rem',
+                    color: 'inherit',
+                    textDecoration: 'none',
+                    }}
                 >
-                  {item}
-                </Button>
-              ))}
-            </Box>
+                    Victor Chen
+                </Typography>
 
-            {/* Mini screen menu Drop down */}
-            <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-              <IconButton
-                size="large"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleOpenNavMenu}
-                color="inherit"
-              >
+
+                {/* Full screen menu */}
+                <Box sx={{ flexGrow: 10,  display: { xs: 'none', md: 'flex' }}} />
+                <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+                {navItems.map((item) => (
+                    <Button
+                        key={item}
+                        onClick={() => {
+                            handleCloseNavMenu()
+                            handleScrollTo(item.toLowerCase())
+                        }}
+                        sx={{ ml: 1, mr: 1, my: 2, color: 'white', display: 'block' }}
+                    >
+                        {item}
+
+                    </Button>
+                ))}
+                </Box>
+
+                
+
+                    {/* Mini screen menu Drop down */}
+                <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                <IconButton
+                    size="large"
+                    aria-label="account of current user"
+                    aria-controls="menu-appbar"
+                    aria-haspopup="true"
+                    onClick={handleOpenNavMenu}
+                    color="inherit"
+                >
                 <MenuIcon />
-              </IconButton>
-              <Menu
+                </IconButton>
+                <Menu
                 id="menu-appbar"
-                anchorEl={elementNav}
+                anchorEl={anchorElNav}
                 anchorOrigin={{
-                  vertical: "bottom",
-                  horizontal: "left",
+                    vertical: 'bottom',
+                    horizontal: 'left',
                 }}
                 keepMounted
                 transformOrigin={{
-                  vertical: "top",
-                  horizontal: "left",
+                    vertical: 'top',
+                    horizontal: 'left',
                 }}
-                open={Boolean(elementNav)}
+                open={Boolean(anchorElNav)}
                 onClose={handleCloseNavMenu}
                 sx={{
-                  display: { xs: "block", md: "none" },
+                    display: { xs: 'block', md: 'none' },
                 }}
-              >
+                >
                 {navItems.map((item) => (
-                  <MenuItem key={item} onClick={handleCloseNavMenu}>
+                    <MenuItem key={item} onClick={handleCloseNavMenu}>
                     <Typography textAlign="center">{item}</Typography>
-                  </MenuItem>
+                    </MenuItem>
                 ))}
-              </Menu>
+                </Menu>
             </Box>
 
-            {/* Mini screen for Header */}
-            <Typography
-              variant="h5"
-              noWrap
-              component="a"
-              href="/"
-              sx={{
-                mr: 2,
-                display: { xs: "flex", md: "none" },
-                flexGrow: 1,
-                fontFamily: "sans-serif",
-                fontWeight: 700,
-                letterSpacing: ".3rem",
-                color: "inherit",
-                textDecoration: "none",
-              }}
-            >
-              Victor Chen
-            </Typography>
-          </Toolbar>
-        </Container>
-      </AppBar>
-    </>
-  );
+                
+                {/* Mini screen for Header */}
+                <Typography
+                    variant="h5"
+                    noWrap
+                    component="a"
+                    href="/"
+                    sx={{
+                    mr: 2,
+                    display: { xs: 'flex', md: 'none' },
+                    flexGrow: 1,
+                    fontFamily: 'sans-serif',
+                    fontWeight: 700,
+                    letterSpacing: '.3rem',
+                    color: 'inherit',
+                    textDecoration: 'none',
+                    }}
+                >
+                    Victor Chen
+                </Typography>
+
+
+                </Toolbar>
+            </Container>
+            </AppBar>
+        </>
+    );
 }
 
 export default NavBar;
