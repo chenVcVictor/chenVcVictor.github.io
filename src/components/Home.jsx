5458a716925bbcb3eebb98fb7c03e8e8ef1ddd1f
Victor Chen <chenvcvictor@gmail.com>
2024-07-19 14:00:36 -0700

    added project card--personal website

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,33 +1,29 @@
+import { Box, Toolbar, Typography } from "@mui/material";
+import "./styles.css";
 
+/*
 
-import { Box, Toolbar, Typography } from '@mui/material';
-import backgroundUrl from "/src/images/Photo_Cropped_LandsEnd_VictorChen_LookingOut.jpeg";
+To Do:
+- Add buttons with logos linking to Email, LinkedIn, Github, maybe Instagram?
+
+*/
 
 function Home() {
-    return (
-      <>
-        <Box id="home" />
-        <Box
-          sx={{
-            backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${backgroundUrl})`,
-            backgroundRepeat: "no-repeat",
-            backgroundSize: "cover",
-            backgroundPosition: 'center',
-            height: '100vh',
-            width: '100%',
-            display: 'flex',
-            flexDirection: 'row',
-            alignItems: 'center', // Align items vertically centered
-            justifyContent: 'center', // Align items horizontally centered
-            backgroundAttachment: 'static',
-          }}        
+  return (
+    <>
+      <Box id="home" />
+      <Box className="homeBackground">
+        <Typography
+          className="homeText"
+          variant="h1"
+          align="center"
+          color="white"
         >
-          <Typography variant='h1' align = 'center' color='white' sx={{ position: 'relative ', width: '70%',  maxWidth: '800px', margin: '0 auto', }}>
-            Hey Welcome! <br /> I am <br /> Victor Chen
-          </Typography>
-        </Box>
-      </>
-    );
-  }
+          Welcome! I am <br /> Victor Chen
+        </Typography>
+      </Box>
+    </>
+  );
+}
 
 export default Home;
