d0f2b0a4b714f5d57a03c2fe9792309465f0fec9
Victor Chen <chenvcvictor@gmail.com>
2024-08-08 22:41:02 -0700

    added google analytics to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import ReactGA from "react-ga";
 import App from "./App.jsx";
+
+ReactGA.initialize("G-505M6Y73CE");
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
